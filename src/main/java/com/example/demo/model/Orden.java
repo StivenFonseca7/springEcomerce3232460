8e5a14b59fdf0bf6f006fa25f884a4e296c05ec3
StivenFonseca7 <frankinfonceca7@gmail.com>
2025-11-07 12:58:22 -0500

    v0.2_configuracionAplication.properties&conexionaBaseDeDatos_StivenF_07112025

--- src/main/java/com/example/demo/model/Orden.java
+++ src/main/java/com/example/demo/model/Orden.java
@@ -1,5 +1,96 @@
 package com.example.demo.model;
 
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orden")
 public class Orden {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String numero;
+	private Date fecahcreacion;
+	private Double total;
+
+	@ManyToOne
+	private Usuario usuario;
+
+	@OneToMany(mappedBy = "orden")
+	private List<DetalleOrden> detalles;
+
+	public Orden() {
+
+	}
+
+	public Orden(Integer id, String numero, Date fecahcreacion, Double total) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.fecahcreacion = fecahcreacion;
+		this.total = total;
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Date getFecahcreacion() {
+		return fecahcreacion;
+	}
+
+	public void setFecahcreacion(Date fecahcreacion) {
+		this.fecahcreacion = fecahcreacion;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<DetalleOrden> getDetalles() {
+		return detalles;
+	}
+
+	public void setDetalles(List<DetalleOrden> detalles) {
+		this.detalles = detalles;
+	}
+
+	@Override
+	public String toString() {
+		return "Orden [id=" + id + ", numero=" + numero + ", fecahcreacion=" + fecahcreacion + ", total=" + total + "]";
+	}
 
 }
--- src/main/java/com/example/demo/model/Producto.java
+++ src/main/java/com/example/demo/model/Producto.java
@@ -1,5 +1,107 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nombre;
+	private double precio;
+	private Integer stock;
+	private String descripcion;
+	private String imagen;
+	
+	@ManyToOne
+	private Usuario usuario;
+
+	public Producto() {
+		
+	}
+
+	public Producto(Integer id, String nombre, double precio, Integer stock, String descripcion, String imagen,
+			Usuario usuario) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+		this.descripcion = descripcion;
+		this.imagen = imagen;
+		this.usuario = usuario;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", stock=" + stock
+				+ ", descripcion=" + descripcion + ", imagen=" + imagen + "]";
+	}
+	
+	
+	
+	
 
 }
